57d37350ae3eac5e378b9fc99abf86055082d9d1
Kyuyeol Park <parkinkon1@naver.com>
2021-08-17 16:15:04 +0900

    m

--- train.py
+++ train.py
@@ -11,33 +11,69 @@ from model.pl_module import SceneTransformer
 
 
 
+def train(cfg, model, train_dataloader, val_dataloader):
+    print('[+] Model training start!')
+    # print(f'Dataset Size - train: {len(train_dataloader)}, valid: {len(val_dataloader)}')
+    from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+    train_callbacks = []
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=None,
+        filename='{epoch}-{val_loss:.2f}',
+        verbose=True,
+        save_last=True,
+        save_top_k=1,
+        monitor='val_loss',
+        mode='min'
+    )
+    train_callbacks.append(checkpoint_callback)
+
+    early_stopping = EarlyStopping(
+        monitor='val_loss',
+        patience=5,
+        verbose=True,
+        mode='min'
+    )
+    train_callbacks.append(early_stopping)
+
+    trainer_args = {
+        'callbacks': train_callbacks,
+        'gpus': cfg.train.n_gpus,
+        'max_epochs': cfg.train.max_epochs,
+    }
+    if cfg.train.ckpt_path != '':
+        trainer_args['resume_from_checkpoint'] = cfg.train.ckpt_path
+    if cfg.train.accelerator != '':
+        trainer_args['accelerator'] = cfg.train.accelerator
+
+    trainer = pl.Trainer(**trainer_args)
+    trainer.fit(model, train_dataloader, val_dataloader)
+    print('[+] Model training end, start testing...')
+    trainer.test(model, val_dataloader)
+    print('[-] Traing and testing ended.')
+
+
+
 @hydra.main(config_path='./conf', config_name='config.yaml')
 def main(cfg):
     os.environ["CUDA_VISIBLE_DEVICES"] = cfg.device_ids
-
-    # GPU_NUM = cfg.device_num
-    # device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
-    # torch.cuda.set_device(device)
-    # if device.type == 'cuda':
-    #     print(torch.cuda.get_device_name(GPU_NUM))
-    #     print('Memory Usage:')
-    #     print('Allocated:', round(torch.cuda.memory_allocated(GPU_NUM)/1024**3,1), 'GB')
-    #     print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM)/1024**3,1), 'GB')
 
     pl.seed_everything(cfg.seed)
     pwd = hydra.utils.get_original_cwd() + '/'
     print('Current Path: ', pwd)
 
-    dataset_train = WaymoDataset(pwd+cfg.dataset.train.tfrecords, pwd+cfg.dataset.train.idxs)
-    dataset_valid = WaymoDataset(pwd+cfg.dataset.valid.tfrecords, pwd+cfg.dataset.valid.idxs)
-    dloader_train = DataLoader(dataset_train, batch_size=cfg.dataset.batchsize, collate_fn=waymo_collate_fn)
-    dloader_valid = DataLoader(dataset_valid, batch_size=cfg.dataset.batchsize, collate_fn=waymo_collate_fn)
+    if cfg.mode == 'train':
+        dataset_train = WaymoDataset(pwd+cfg.dataset.train.tfrecords, pwd+cfg.dataset.train.idxs)
+        dataset_valid = WaymoDataset(pwd+cfg.dataset.valid.tfrecords, pwd+cfg.dataset.valid.idxs)
+        dloader_train = DataLoader(dataset_train, batch_size=cfg.dataset.batchsize, num_workers=cfg.dataset.num_workers, collate_fn=waymo_collate_fn)
+        dloader_valid = DataLoader(dataset_valid, batch_size=cfg.dataset.batchsize, num_workers=cfg.dataset.num_workers, collate_fn=waymo_collate_fn)
 
-    model = SceneTransformer(None, cfg.model.in_feature_dim, cfg.model.in_dynamic_rg_dim, cfg.model.in_static_rg_dim,
-                                cfg.model.time_steps, cfg.model.feature_dim, cfg.model.head_num, cfg.model.k, cfg.model.F)
+        model = SceneTransformer(None, cfg.model.in_feature_dim, cfg.model.in_dynamic_rg_dim, cfg.model.in_static_rg_dim,
+                                    cfg.model.time_steps, cfg.model.feature_dim, cfg.model.head_num, cfg.model.k, cfg.model.F)
+        train(cfg, model, dloader_train, dloader_valid)
+    else:
+        raise NotImplementedError(f'Yet Implemented mode: {cfg.mode}')
 
-    trainer = pl.Trainer(max_epochs=cfg.max_epochs, gpus=cfg.device_num)
-    trainer.fit(model, dloader_train, dloader_valid)
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
